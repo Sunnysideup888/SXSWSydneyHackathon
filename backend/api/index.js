c5fab8246dbdf6dbbdd6ec4c2d5042ec56297296
Sunny Chen <sunnychenaustralia@gmail.com>
2025-10-17 00:30:29 +1100

    Using express router

--- backend/api/index.js
+++ backend/api/index.js
@@ -6,31 +6,32 @@ const { createClient } = require('@supabase/supabase-js');
 const app = express();
 const port = 3001;
 
+const apiRouter = express.Router();
+
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_ANON_KEY;
-console.log(supabaseUrl, supabaseKey)
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.json({ message: 'This test is a test if the backend works' });
+apiRouter.get('/', (req, res) => {
+    res.json({ message: 'Hello World!' });
 });
 
-app.get('/tasks', async (req, res) => {
+apiRouter.get('/tasks', async (req, res) => {
     const { data, error } = await supabase
         .from('tasks')
         .select('*');
 
-    console.log('This is the data ' + JSON.stringify(data, null, 2))
-    console.log('This is the error ' + error)
-
     if (error) {
+        console.error('Supabase error:', error);
         return res.status(500).json({ error: error.message });
     }
     res.status(200).json(data);
 });
+
+app.use('', apiRouter);
 
 if (process.env.NODE_ENV !== 'production') {
     app.listen(port, () => {
