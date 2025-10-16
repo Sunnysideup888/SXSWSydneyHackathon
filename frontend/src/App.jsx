6ad56478d2657e32675fc91177ff9cc8e835b398
Sunny Chen <z5587681@ad.unsw.edu.au>
2025-10-16 17:46:18 +1100

    Frontend setup even more

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,9 @@
 import { useState } from 'react'
 
 function App() {
-    const [tasks, setTasks] = useState([])
-    const [msg, setMsg] = useState("Loading...")
-
   return (
     <>
-        <div className={"flex flex-row bg-amber-50 justify-around"}>
-            <div>
-                Hello
-            </div>
-            <div>
-                World
-            </div>
-        </div>
+        <p className={"flex flex-row p-4 bg-amber-50"}>Hello World!</p>
     </>
   )
 }
